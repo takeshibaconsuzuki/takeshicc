d8b78381b551ff9af0d9ee47119105747deefed1
Takeshi Suzuki <takeshibaconsuzuki@gmail.com>
2026-05-15 19:24:05 -0700

    Revert "Shared server"
    This reverts commit 49c5f8532d6af4f7f50caf684ff1e11d323afdb2.

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,12 @@
 import * as vscode from 'vscode';
 import { applyLayout } from './applyLayout';
 import { registerPasteFileRef } from './pasteFileRef';
-import { SharedServerConnection } from './sharedServer';
-
-let sharedServer: SharedServerConnection | undefined;
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand('takeshicc.applyLayout', () => applyLayout(context))
   );
   registerPasteFileRef(context);
-
-  const log = vscode.window.createOutputChannel('Takeshicc');
-  context.subscriptions.push(log);
-  sharedServer = new SharedServerConnection(context, log);
-  void sharedServer.start();
 }
 
-export function deactivate() {
-  sharedServer?.dispose();
-  sharedServer = undefined;
-}
+export function deactivate() {}
